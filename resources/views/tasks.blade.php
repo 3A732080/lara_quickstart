2ad5f05da503567ebaf8530bb889eed4d4c8f7d6
3A732080 <tiffany89031176@gmail.com>
2020-11-18 01:06:38 +0800

    刪除任務: 修改task視圖及第三個路由

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -1,5 +1,3 @@
-// resources/views/tasks.blade.php
-
 @extends('layouts.app')
 
 @section('content')
@@ -61,6 +59,14 @@
 
                             <td>
                                 <!-- 代辦：刪除按鈕 -->
+                            </td>
+                            <td>
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+
+                                    <button>刪除任務</button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
